c902bec4a72495ccbe96d2caa07ea58060cd7564
Steve Wilson <stw4198@aol.com>
2021-02-08 16:33:55 +0000

    charge-based fitting option included

--- plotting.py
+++ plotting.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 from numpy.polynomial.polynomial import polyfit
+from scipy.optimize import curve_fit
 
 Tol_bright = ['#4477AA','#66CCEE','#228833',
                      '#CCBB44','#EE6677','#AA3377','#BBBBBB','k']
@@ -10,9 +11,9 @@ CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
                   '#f781bf', '#a65628', '#984ea3', 
                   '#999999', '#e41a1c', '#dede00']
 
-def value_extraction(media,interval,tags):
+def value_extraction(media,tags):
 
-	stats = open("../%s_%f%s/stats_%s.txt" % (media,interval,tags,media), 'r')
+	stats = open("../%s%s/stats_%s.txt" % (media,tags,media), 'r')
 	stats_lines = stats.readlines()
 
 	RES = stats_lines[14::9]
@@ -72,48 +73,59 @@ def value_extraction(media,interval,tags):
 
 	return(resolution,resolution_err,en,en_err)
 
+def fit(e,a,b,c):
+	return(a/np.sqrt(e) + b + c/e)
+
 interval = 0.25
-medium_type = ''
-pos = 'letterbox'
+pos = 'charge'
 
 if pos == 'off':
 
-	resolution_1,resolution_err_1,energy_1,energy_err_1 = value_extraction("wbls_1pc_baseline",interval,'')
-	resolution_3,resolution_err_3,energy_3,energy_err_3 = value_extraction("wbls_3pc_baseline",interval,'')
-	resolution_5,resolution_err_5,energy_5,energy_err_5 = value_extraction("wbls_5pc_baseline",interval,'')
+	resolution_1,resolution_err_1,energy_1,energy_err_1 = value_extraction("wbls_1pc_baseline",'')
+	resolution_3,resolution_err_3,energy_3,energy_err_3 = value_extraction("wbls_3pc_baseline",'')
+	resolution_5,resolution_err_5,energy_5,energy_err_5 = value_extraction("wbls_5pc_baseline",'')
 
 elif pos == 'on':
 
-	resolution_1_0,resolution_err_1_0,energy_1_0,energy_err_1_0 = value_extraction("wbls_1pc",interval,'_centre')
-	resolution_1_1000,resolution_err_1_1000,energy_1_1000,energy_err_1_1000 = value_extraction("wbls_1pc",interval,'_1000')
-	resolution_1_2000,resolution_err_1_2000,energy_1_2000,energy_err_1_2000 = value_extraction("wbls_1pc",interval,'_2000')
-	resolution_1_3000,resolution_err_1_3000,energy_1_3000,energy_err_1_3000 = value_extraction("wbls_1pc",interval,'_3000')
-	resolution_1_4000,resolution_err_1_4000,energy_1_4000,energy_err_1_4000 = value_extraction("wbls_1pc",interval,'_4000')
-	resolution_1_5000,resolution_err_1_5000,energy_1_5000,energy_err_1_5000 = value_extraction("wbls_1pc",interval,'_5000')
-	resolution_1_6000,resolution_err_1_6000,energy_1_6000,energy_err_1_6000 = value_extraction("wbls_1pc",interval,'_6000')
+	resolution_1_0,resolution_err_1_0,energy_1_0,energy_err_1_0 = value_extraction("wbls_1pc",'_centre')
+	resolution_1_1000,resolution_err_1_1000,energy_1_1000,energy_err_1_1000 = value_extraction("wbls_1pc",'_1000')
+	resolution_1_2000,resolution_err_1_2000,energy_1_2000,energy_err_1_2000 = value_extraction("wbls_1pc",'_2000')
+	resolution_1_3000,resolution_err_1_3000,energy_1_3000,energy_err_1_3000 = value_extraction("wbls_1pc",'_3000')
+	resolution_1_4000,resolution_err_1_4000,energy_1_4000,energy_err_1_4000 = value_extraction("wbls_1pc",'_4000')
+	resolution_1_5000,resolution_err_1_5000,energy_1_5000,energy_err_1_5000 = value_extraction("wbls_1pc",'_5000')
+	resolution_1_6000,resolution_err_1_6000,energy_1_6000,energy_err_1_6000 = value_extraction("wbls_1pc",'_6000')
 
-	resolution_3_0,resolution_err_3_0,energy_3_0,energy_err_3_0 = value_extraction("wbls_3pc",interval,'_centre')
-	resolution_3_1000,resolution_err_3_1000,energy_3_1000,energy_err_3_1000 = value_extraction("wbls_3pc",interval,'_1000')
-	resolution_3_2000,resolution_err_3_2000,energy_3_2000,energy_err_3_2000 = value_extraction("wbls_3pc",interval,'_2000')
-	resolution_3_3000,resolution_err_3_3000,energy_3_3000,energy_err_3_3000 = value_extraction("wbls_3pc",interval,'_3000')
-	resolution_3_4000,resolution_err_3_4000,energy_3_4000,energy_err_3_4000 = value_extraction("wbls_3pc",interval,'_4000')
-	resolution_3_5000,resolution_err_3_5000,energy_3_5000,energy_err_3_5000 = value_extraction("wbls_3pc",interval,'_5000')
-	resolution_3_6000,resolution_err_3_6000,energy_3_6000,energy_err_3_6000 = value_extraction("wbls_3pc",interval,'_6000')
+	resolution_3_0,resolution_err_3_0,energy_3_0,energy_err_3_0 = value_extraction("wbls_3pc",'_centre')
+	resolution_3_1000,resolution_err_3_1000,energy_3_1000,energy_err_3_1000 = value_extraction("wbls_3pc",'_1000')
+	resolution_3_2000,resolution_err_3_2000,energy_3_2000,energy_err_3_2000 = value_extraction("wbls_3pc",'_2000')
+	resolution_3_3000,resolution_err_3_3000,energy_3_3000,energy_err_3_3000 = value_extraction("wbls_3pc",'_3000')
+	resolution_3_4000,resolution_err_3_4000,energy_3_4000,energy_err_3_4000 = value_extraction("wbls_3pc",'_4000')
+	resolution_3_5000,resolution_err_3_5000,energy_3_5000,energy_err_3_5000 = value_extraction("wbls_3pc",'_5000')
+	resolution_3_6000,resolution_err_3_6000,energy_3_6000,energy_err_3_6000 = value_extraction("wbls_3pc",'_6000')
 
-	resolution_5_0,resolution_err_5_0,energy_5_0,energy_err_5_0 = value_extraction("wbls_5pc",interval,'_centre')
-	resolution_5_1000,resolution_err_5_1000,energy_5_1000,energy_err_5_1000 = value_extraction("wbls_5pc",interval,'_1000')
-	resolution_5_2000,resolution_err_5_2000,energy_5_2000,energy_err_5_2000 = value_extraction("wbls_5pc",interval,'_2000')
-	resolution_5_3000,resolution_err_5_3000,energy_5_3000,energy_err_5_3000 = value_extraction("wbls_5pc",interval,'_3000')
-	resolution_5_4000,resolution_err_5_4000,energy_5_4000,energy_err_5_4000 = value_extraction("wbls_5pc",interval,'_4000')
-	resolution_5_5000,resolution_err_5_5000,energy_5_5000,energy_err_5_5000 = value_extraction("wbls_5pc",interval,'_5000')
-	resolution_5_6000,resolution_err_5_6000,energy_5_6000,energy_err_5_6000 = value_extraction("wbls_5pc",interval,'_6000')
+	resolution_5_0,resolution_err_5_0,energy_5_0,energy_err_5_0 = value_extraction("wbls_5pc",'_centre')
+	resolution_5_1000,resolution_err_5_1000,energy_5_1000,energy_err_5_1000 = value_extraction("wbls_5pc",'_1000')
+	resolution_5_2000,resolution_err_5_2000,energy_5_2000,energy_err_5_2000 = value_extraction("wbls_5pc",'_2000')
+	resolution_5_3000,resolution_err_5_3000,energy_5_3000,energy_err_5_3000 = value_extraction("wbls_5pc",'_3000')
+	resolution_5_4000,resolution_err_5_4000,energy_5_4000,energy_err_5_4000 = value_extraction("wbls_5pc",'_4000')
+	resolution_5_5000,resolution_err_5_5000,energy_5_5000,energy_err_5_5000 = value_extraction("wbls_5pc",'_5000')
+	resolution_5_6000,resolution_err_5_6000,energy_5_6000,energy_err_5_6000 = value_extraction("wbls_5pc",'_6000')
 	
 elif pos == 'centre':
 
-	resolution_1_0,resolution_err_1_0,energy_1_0,energy_err_1_0 = value_extraction("wbls_1pc",interval,'_centre')
-	resolution_3_0,resolution_err_3_0,energy_3_0,energy_err_3_0 = value_extraction("wbls_3pc",interval,'_centre')
-	resolution_5_0,resolution_err_5_0,energy_5_0,energy_err_5_0 = value_extraction("wbls_5pc",interval,'_centre')
-	
+	resolution_1_0,resolution_err_1_0,energy_1_0,energy_err_1_0 = value_extraction("wbls_1pc",'_centre')
+	resolution_3_0,resolution_err_3_0,energy_3_0,energy_err_3_0 = value_extraction("wbls_3pc",'_centre')
+	resolution_5_0,resolution_err_5_0,energy_5_0,energy_err_5_0 = value_extraction("wbls_5pc",'_centre')
+
+elif pos == 'charge':
+
+	resolution_1,resolution_err_1,energy_1,energy_err_1 = value_extraction("wbls_1pc_baseline",'')
+	resolution_3,resolution_err_3,energy_3,energy_err_3 = value_extraction("wbls_3pc_baseline",'')
+	resolution_5,resolution_err_5,energy_5,energy_err_5 = value_extraction("wbls_5pc_baseline",'')
+	resolution_1_q,resolution_err_1_q,energy_1_q,energy_err_1_q = value_extraction("wbls_1pc_baseline",'_charge')
+	resolution_3_q,resolution_err_3_q,energy_3_q,energy_err_3_q = value_extraction("wbls_3pc_baseline",'_charge')
+	resolution_5_q,resolution_err_5_q,energy_5_q,energy_err_5_q = value_extraction("wbls_5pc_baseline",'_charge')
+
 else:
 
 	print('\nNo position-based energy reconstruction\n')	
@@ -257,12 +269,12 @@ elif pos == 'letterbox':
 
 	interval = 0.5
 
-	res_8_50_8_10pct,res_err_8_50_8_10pct,en_8_50_8_10pct,en_err_8_50_8_10pct = value_extraction("8_50_8_10pct",interval,'')
-	res_8_50_8_15pct,res_err_8_50_8_15pct,en_8_50_8_15pct,en_err_8_50_8_15pct = value_extraction("8_50_8_15pct",interval,'')
-	res_8_50_8_20pct,res_err_8_50_8_20pct,en_8_50_8_20pct,en_err_8_50_8_20pct = value_extraction("8_50_8_20pct",interval,'')
-	res_8_80_8_10pct,res_err_8_80_8_10pct,en_8_80_8_10pct,en_err_8_80_8_10pct = value_extraction("8_80_8_10pct",interval,'')
-	res_8_80_8_15pct,res_err_8_80_8_15pct,en_8_80_8_15pct,en_err_8_80_8_15pct = value_extraction("8_80_8_15pct",interval,'')
-	res_8_80_8_20pct,res_err_8_80_8_20pct,en_8_80_8_20pct,en_err_8_80_8_20pct = value_extraction("8_80_8_20pct",interval,'')
+	res_8_50_8_10pct,res_err_8_50_8_10pct,en_8_50_8_10pct,en_err_8_50_8_10pct = value_extraction("8_50_8_10pct",'')
+	res_8_50_8_15pct,res_err_8_50_8_15pct,en_8_50_8_15pct,en_err_8_50_8_15pct = value_extraction("8_50_8_15pct",'')
+	res_8_50_8_20pct,res_err_8_50_8_20pct,en_8_50_8_20pct,en_err_8_50_8_20pct = value_extraction("8_50_8_20pct",'')
+	res_8_80_8_10pct,res_err_8_80_8_10pct,en_8_80_8_10pct,en_err_8_80_8_10pct = value_extraction("8_80_8_10pct",'')
+	res_8_80_8_15pct,res_err_8_80_8_15pct,en_8_80_8_15pct,en_err_8_80_8_15pct = value_extraction("8_80_8_15pct",'')
+	res_8_80_8_20pct,res_err_8_80_8_20pct,en_8_80_8_20pct,en_err_8_80_8_20pct = value_extraction("8_80_8_20pct",'')
 	
 	energy = np.arange(interval,10.+interval,interval)
 	energy_err = interval/np.sqrt(12)
@@ -326,12 +338,12 @@ elif pos == 'cylinder':
 	energy = np.arange(interval,10.+interval,interval)
 	energy_err = interval/np.sqrt(12)
 
-	res_10_10_10pct, res_err_10_10_10pct, en_10_10_10pct, en_err_10_10_10pct = value_extraction("10_10_10pct",interval,'')
-	res_10_10_15pct, res_err_10_10_15pct, en_10_10_15pct, en_err_10_10_15pct = value_extraction("10_10_15pct",interval,'')
-	res_10_10_20pct, res_err_10_10_20pct, en_10_10_20pct, en_err_10_10_20pct = value_extraction("10_10_20pct",interval,'')
-	res_12_12_10pct, res_err_12_12_10pct, en_12_12_10pct, en_err_20_20_10pct = value_extraction("12_12_10pct",interval,'')
-	res_12_12_15pct, res_err_12_12_15pct, en_12_12_15pct, en_err_20_20_15pct = value_extraction("12_12_15pct",interval,'')
-	res_12_12_20pct, res_err_12_12_20pct, en_12_12_20pct, en_err_20_20_20pct = value_extraction("12_12_20pct",interval,'')
+	res_10_10_10pct, res_err_10_10_10pct, en_10_10_10pct, en_err_10_10_10pct = value_extraction("10_10_10pct",'')
+	res_10_10_15pct, res_err_10_10_15pct, en_10_10_15pct, en_err_10_10_15pct = value_extraction("10_10_15pct",'')
+	res_10_10_20pct, res_err_10_10_20pct, en_10_10_20pct, en_err_10_10_20pct = value_extraction("10_10_20pct",'')
+	res_12_12_10pct, res_err_12_12_10pct, en_12_12_10pct, en_err_20_20_10pct = value_extraction("12_12_10pct",'')
+	res_12_12_15pct, res_err_12_12_15pct, en_12_12_15pct, en_err_20_20_15pct = value_extraction("12_12_15pct",'')
+	res_12_12_20pct, res_err_12_12_20pct, en_12_12_20pct, en_err_20_20_20pct = value_extraction("12_12_20pct",'')
 	
 	plt.errorbar(energy[1:-1],res_10_10_10pct[1:-1],yerr=res_err_10_10_10pct[1:-1],xerr=energy_err,linestyle='none',color=Tol_bright[0],label='10% PC')
 	plt.errorbar(energy[1:-1],res_10_10_15pct[1:-1],yerr=res_err_10_10_15pct[1:-1],xerr=energy_err,linestyle='none',color=Tol_bright[3],label='15% PC')
@@ -389,3 +401,77 @@ elif pos == 'cylinder':
 	#plt.ylim(0,2*res_10_10_20pct[1])
 	plt.savefig("../20pct_cylinder.png")
 	plt.show()
+	
+elif pos == 'charge':
+
+	plt.errorbar(energy,resolution_1,yerr=resolution_err_1,xerr=energy_err,linestyle='none',color=Tol_bright[0],label="WbLS 1% (nhits)")
+	plt.errorbar(energy,resolution_3,yerr=resolution_err_3,xerr=energy_err,linestyle='none',color=Tol_bright[3],label="WbLS 3% (nhits)")
+	plt.errorbar(energy,resolution_5,yerr=resolution_err_5,xerr=energy_err,linestyle='none',color=Tol_bright[7],label="WbLS 5% (nhits)")
+	plt.xlim(energy[2]-0.125,energy[-1]+0.125)
+	plt.ylim(0,resolution_1[2]+resolution_err_1[2]+0.05)
+	plt.xlabel("Kinetic Energy [MeV]")
+	plt.ylabel("Resolution [\u03C3/E]")
+	plt.title("Electrons in Baseline WATCHMAN Detector")
+	plt.legend()
+	plt.grid()
+	plt.savefig("../nhits_whole.png")
+	plt.show()
+	
+	plt.errorbar(energy,resolution_1_q,yerr=resolution_err_1_q,xerr=energy_err,linestyle='none',color=Tol_bright[0],label="WbLS 1% (charge)")
+	plt.errorbar(energy,resolution_3_q,yerr=resolution_err_3_q,xerr=energy_err,linestyle='none',color=Tol_bright[3],label="WbLS 3% (charge)")
+	plt.errorbar(energy,resolution_5_q,yerr=resolution_err_5_q,xerr=energy_err,linestyle='none',color=Tol_bright[7],label="WbLS 5% (charge)")
+	plt.xlim(energy[2]-0.125,energy[-1]+0.125)
+	plt.ylim(0,resolution_1[2]+resolution_err_1[2]+0.05)
+	plt.xlabel("Kinetic Energy [MeV]")
+	plt.ylabel("Resolution [\u03C3/E]")
+	plt.title("Electrons in Baseline WATCHMAN Detector")
+	plt.legend()
+	plt.grid()
+	plt.savefig("../charge_whole.png")
+	plt.show()
+	
+	popt, pcov = curve_fit(fit, energy, resolution_1_q)
+	
+	plt.errorbar(energy,resolution_1,yerr=resolution_err_1,xerr=energy_err,linestyle='none',color=Tol_bright[3],label="WbLS 1% (nhits)")
+	plt.errorbar(energy,resolution_1_q,yerr=resolution_err_1_q,xerr=energy_err,linestyle='none',color=Tol_bright[7],label="WbLS 1% (charge)")
+	plt.errorbar(energy,fit(energy,*popt),color=Tol_bright[0],label='fit: %5.3f/$\sqrt{E}$ + %5.3f + %5.3f/E' % tuple(popt))
+	plt.xlim(energy[2]-0.125,energy[-1]+0.125)
+	plt.ylim(0,resolution_1[2]+resolution_err_1[2]+0.05)
+	plt.xlabel("Kinetic Energy [MeV]")
+	plt.ylabel("Resolution [\u03C3/E]")
+	plt.title("Electrons in Baseline WATCHMAN Detector")
+	plt.legend()
+	plt.grid()
+	plt.savefig("../charge_vs_nhits_1pct_whole.png")
+	plt.show()
+	
+	popt, pcov = curve_fit(fit, energy, resolution_3_q)
+	
+	plt.errorbar(energy,resolution_3,yerr=resolution_err_3,xerr=energy_err,linestyle='none',color=Tol_bright[3],label="WbLS 3% (nhits)")
+	plt.errorbar(energy,resolution_3_q,yerr=resolution_err_3_q,xerr=energy_err,linestyle='none',color=Tol_bright[7],label="WbLS 3% (charge)")
+	plt.errorbar(energy,fit(energy,*popt),color=Tol_bright[0],label='fit: %5.3f/$\sqrt{E}$ + %5.3f + %5.3f/E' % tuple(popt))
+	plt.xlim(energy[2]-0.125,energy[-1]+0.125)
+	plt.ylim(0,resolution_1[2]+resolution_err_1[2]+0.05)
+	plt.xlabel("Kinetic Energy [MeV]")
+	plt.ylabel("Resolution [\u03C3/E]")
+	plt.title("Electrons in Baseline WATCHMAN Detector")
+	plt.legend()
+	plt.grid()
+	plt.savefig("../charge_vs_nhits_3pct_whole.png")
+	plt.show()
+	
+	popt, pcov = curve_fit(fit, energy, resolution_5_q)
+
+	plt.errorbar(energy,resolution_5,yerr=resolution_err_5,xerr=energy_err,linestyle='none',color=Tol_bright[3],label="WbLS 5% (nhits)")
+	plt.errorbar(energy,resolution_5_q,yerr=resolution_err_5_q,xerr=energy_err,linestyle='none',color=Tol_bright[7],label="WbLS 5% (charge)")
+	plt.errorbar(energy,fit(energy,*popt),color=Tol_bright[0],label='fit: %5.3f/$\sqrt{E}$ + %5.3f + %5.3f/E' % tuple(popt))
+	plt.xlim(energy[2]-0.125,energy[-1]+0.125)
+	plt.ylim(0,resolution_1[2]+resolution_err_1[2]+0.05)
+	plt.xlabel("Kinetic Energy [MeV]")
+	plt.ylabel("Resolution [\u03C3/E]")
+	plt.title("Electrons in Baseline WATCHMAN Detector")
+	plt.legend()
+	plt.grid()
+	plt.savefig("../charge_vs_nhits_5pct_whole.png")
+	plt.show()
+
